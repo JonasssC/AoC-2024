92f1a4df30555bc3fa9c76a779589277885f80e3
Jonas Couwberghs <jonas.couwberghs@student.kuleuven.be>
2024-12-02 06:26:57 +0100

    2024 day 2

---
+++ src/y2024/d02/Main.kt
@@ -0,0 +1,27 @@
+package y2024.d02
+
+import lib.readStr
+import kotlin.math.abs
+
+fun main() {
+    val input = readStr(2024, 2).lines().map { it.split(" ").map { n -> n.toInt() } }
+
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+}
+
+fun part1(input: List<List<Int>>): Int =
+    input.count {
+        (it == it.sorted() || it == it.sortedDescending())
+                && it.zipWithNext().all { (a, b) -> abs(a - b) in 1..3 }
+    }
+
+fun part2(input: List<List<Int>>): Int =
+    input.count {
+        (0..it.size).map { i ->
+            it.filterIndexed { j, _ -> j != i }
+        }.any { l ->
+            (l == l.sorted() || l == l.sortedDescending())
+                    && l.zipWithNext().all { (a, b) -> abs(a - b) in 1..3 }
+        }
+    }
