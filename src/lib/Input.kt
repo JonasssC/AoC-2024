8f7307ea3b6e99c2e531dc591bb2ac1d569d1ac5
Jonas Couwberghs <jonas.couwberghs@student.kuleuven.be>
2024-12-04 06:29:04 +0100

    2024 day 4

--- src/lib/Input.kt
+++ src/lib/Input.kt
@@ -3,5 +3,5 @@ package lib
 import java.io.File
 
 fun readStr(year: Int, day: Int): String
-    = File(String.format("out/production/AoC-2024/y%d/d%02d/input.txt", year, day))
+    = File(String.format("input/y%d/d%02d/input.txt", year, day))
         .readText()
---
+++ src/y2024/d04/Main.kt
@@ -0,0 +1,58 @@
+package y2024.d04
+
+import lib.readStr
+
+typealias Input = List<String>
+typealias Output = Int
+
+fun main() {
+    val input = readStr(2024, 4).lines()
+
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+}
+
+val directions = listOf(
+    -1 to -1,
+    -1 to 0,
+    -1 to 1,
+    0 to -1,
+    0 to 1,
+    1 to -1,
+    1 to 0,
+    1 to 1,
+)
+
+fun inField(input: Input, row: Int, col: Int) =
+    0 <= row && row < input.size && 0 <= col && col < input[row].length
+
+fun part1(input: Input): Output {
+    val xCoords = input.flatMapIndexed { row, line ->
+        line.withIndex().filter { it.value == 'X' }.map { row to it.index }
+    }
+
+    var count = 0
+    for ((row, col) in xCoords) {
+        for ((dRow, dCol) in directions) {
+            if (inField(input, row + 3 * dRow, col + 3 * dCol)) {
+                if (input[row + dRow][col + dCol] == 'M'
+                    && input[row + 2 * dRow][col + 2 * dCol] == 'A'
+                    && input[row + 3 * dRow][col + 3 * dCol] == 'S') {
+                    count++
+                }
+            }
+        }
+    }
+
+    return count
+}
+
+fun part2(input: Input): Output =
+    input.flatMapIndexed { row, line ->
+        line.withIndex().filter { it.value == 'A' }.map { row to it.index }
+    }.filter { (row, col) ->
+        inField(input, row - 1, col - 1)
+                && inField(input, row + 1, col + 1)
+                && listOf(input[row + 1][col + 1], input[row - 1][col - 1]).sorted() == listOf('M', 'S')
+                && listOf(input[row + 1][col - 1], input[row - 1][col + 1]).sorted() == listOf('M', 'S')
+    }.size
